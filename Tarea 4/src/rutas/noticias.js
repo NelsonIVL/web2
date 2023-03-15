68ac1b3661674e9f088861264462d25a23942351
NelsonIVL <102566545+NelsonIVL@users.noreply.github.com>
2023-03-15 13:27:13 -0600

    Delete Tarea 4 directory

--- Tarea 4/src/rutas/noticias.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controladorNoticias = require('../controllers/noticias');
-
-//Agregar las rutas para noticia/
-
-router.get('',controladorNoticias.buscar); //Ejecuta la función del controlador
-
-module.exports = router;
